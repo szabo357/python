17d3657e0f9cb1c2d6a9aac82ab1ce61d5902a1c
szabo357 <szabo357@gmail.com>
2023-10-14 17:57:33 -0600

    Los filtros ya trabajan

---
+++ Apps/Apps-Flet/todo2_app.py
@@ -0,0 +1,184 @@
+# Filtering Feature will be added to the App using
+# Tab control.
+
+import flet as ft
+
+class Task(ft.UserControl):
+    def __init__(self, task_name,task_status_change, task_delete):
+        super().__init__()
+        self.completed = False
+        self.task_name = task_name
+        self.task_status_change = task_status_change
+        self.task_delete = task_delete
+
+    def build(self):
+        self.display_task = ft.Checkbox(
+            value=False, label=self.task_name,on_change=self.status_changed)
+        self.edit_name = ft.TextField(expand=1)
+        
+        self.display_view = ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.display_task,
+                ft.Row(
+                    spacing=0,
+                    controls=[
+                        ft.IconButton(
+                            icon=ft.icons.CREATE_OUTLINED,
+                            tooltip="Edit To-Do",
+                            on_click=self.edit_clicked,
+                        ),
+                        ft.IconButton(
+                            icon=ft.icons.DELETE_OUTLINED,
+                            tooltip="Delete To-Do",
+                            on_click=self.delete_clicked,
+                        ),
+                    ],
+                ),
+            ],
+        )
+
+        self.edit_view = ft.Row(
+            visible= False,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.edit_name,
+                ft.IconButton(
+                    icon= ft.icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=ft.colors.GREEN,
+                    tooltip="Update To-Do",
+                    on_click = self.save_clicked,
+                ),
+            ],
+        )
+        return ft.Column(controls=[self.display_view,self.edit_view])
+    
+    def edit_clicked(self,e):
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+    
+    def save_clicked(self,e):
+        self.display_task.label= self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        self.update()
+        
+    def delete_clicked(self,e):
+        self.task_delete(self)
+    
+    def status_changed(self,e):
+        self.completed = self.display_task.value
+        self.task_status_change(self)
+
+class TodoApp(ft.UserControl):
+    def build(self):
+        self.tasks = []
+        self.new_task = ft.TextField(hint_text="¿Qué tareas hay que hacer?",expand=True)
+        self.tasks = ft.Column()
+
+        self.filter = ft.Tabs(
+            selected_index=0,
+            on_change=self.tabs_changed,
+            tabs=[ft.Tab(text="all"),ft.Tab(text="active"),ft.Tab(text="completed")],
+        )
+
+        self.items_left = ft.Text("0 items left")
+
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        ft.FloatingActionButton(icon=ft.icons.ADD,on_click=self.add_clicked),
+                    ],                    
+                ),
+                ft.Column(
+                    spacing=25,
+                    controls=[
+                        self.filter,
+                        self.tasks,
+                    ],
+                ),
+            ],
+        )
+    ''' 
+    def build(self):
+        self.new_task = ft.TextField(hint_text="¿Qué tareas hay que hacer?",expand=True)
+        self.tasks = ft.Column()
+
+
+        # application's root control (i.e "view") containing all other controls
+        return ft.Column(
+            width=600,
+            controls=[
+                ft.Row(
+                    controls=[
+                        self.new_task,
+                        ft.FloatingActionButton(icon=ft.icons.ADD, on_click=self.add_clicked),
+                    ],
+                ),
+                self.tasks,
+            ],
+        ) '''    
+
+    def add_clicked(self,e):
+        task = Task(self.new_task.value,self.task_status_change,self.task_delete)
+       # task = Task(self.new_task.value,self.task_delete)
+        self.tasks.controls.append(task)
+        self.new_task.value =""
+        self.update() 
+    
+    def task_status_change(self,task):
+        self.update()
+
+    def task_delete(self,task):
+        self.tasks.controls.remove(task)
+        self.update()
+    
+    def tabs_changed(self,e):
+        self.update()
+
+    def clear_clicked(self,e):
+        for task in self.tasks.controls[:]:
+            if task.completed:
+                self.task_delete(task)
+
+    # overrides update() method which iterates through all tasks
+    # and updates their visible property depending on the status of the task.
+    def update(self):
+        status = self.filter.tabs[self.filter.selected_index].text
+        count = 0
+        for task in self.tasks.controls:
+           task.visible = (
+                status == "all"
+                or (status == "active" and task.completed == False)
+                or (status == "completed" and task.completed)
+           )
+           if not task.completed:
+               count+= 1
+        self.items_left.value = f"{count} active item(s) left"
+        super().update()
+    
+
+
+def main(page: ft.Page):
+    page.title = "ToDo App"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
+
+    #create application instance
+    todo = TodoApp()
+
+    #add application's root control to the page
+    page.add(todo)
+
+
+ft.app(target=main)
+#ft.app(target=to_do,view=ft.AppView.WEB_BROWSER)
+
+#Theres work in progress
