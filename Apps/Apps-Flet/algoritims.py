d3c75d1303bbd035f61d404c46f5eabe705c66cd
szabo357 <szabo357@gmail.com>
2023-10-16 22:11:59 -0600

    Ordenamiento Burbuja Flet

--- Apps/Apps-Flet/algoritims.py
+++ Apps/Apps-Flet/algoritims.py
@@ -3,13 +3,13 @@ import random
 import time
 
 def main(page: ft.Page):
-    
-    def balls(count):
+
+    def contenedores(count):
         items= []
         for _  in range(count):
             items.append(    
                 ft.Container(
-                    content=ft.Text( value=str(random.randint(1,200))),
+                    content=ft.Text( value=random.randint(1,200)),
                     alignment=ft.alignment.center,
                     width=50,
                     height=50,
@@ -18,24 +18,22 @@ def main(page: ft.Page):
                 )
             )
         return items
+        
+    def OrdenamientoBurbuja(arr):
+        n = len(arr)
+        for i in range(n):
+            for j in range(0, n-i-1 ):
+                arr[j].bgcolor=ft.colors.YELLOW
+                arr[j + 1].bgcolor=ft.colors.YELLOW
+                time.sleep(2)
+                page.update()
+                if arr[j].content.value > arr[j + 1].content.value :
+                    arr[j] , arr[j + 1] = arr[j + 1] , arr[j]    
+                arr[j].bgcolor=ft.colors.ORANGE
+                arr[j + 1].bgcolor=ft.colors.ORANGE
+            arr[n-i-1].bgcolor=ft.colors.GREEN
+        page.update()
+
+    OrdenamientoBurbuja(page.add(ft.Row(contenedores)))
     
-    pelotas = balls(10)
-
-    page.add(ft.Row(pelotas))
-    n = len(pelotas)
-
-    for i in range(n):
-        for j in range(0, n-i-1):
-            pelotas[j].bgcolor=ft.colors.YELLOW
-            pelotas[j + 1].bgcolor=ft.colors.YELLOW
-            time.sleep(2)
-            page.update()
-            if pelotas[j].content.value > pelotas[j + 1].content.value:
-                pelotas[j] , pelotas[j + 1] = pelotas[j + 1] , pelotas[j]    
-            pelotas[j].bgcolor=ft.colors.ORANGE
-            pelotas[j + 1].bgcolor=ft.colors.ORANGE
-        pelotas[n-i-1].bgcolor=ft.colors.GREEN
-    page.update()
-
-
 ft.app(target=main)
