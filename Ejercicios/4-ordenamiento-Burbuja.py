af0983da0330ad88917f70b8db09b98f38c852bd
szabo357 <szabo357@gmail.com>
2023-10-16 20:31:24 -0600

    Ejercicios ordenamiento burbuja

---
+++ Ejercicios/4-ordenamiento-Burbuja.py
@@ -0,0 +1,8 @@
+'''4-Implementa el algoritmo de ordenamiento de Burbuja imprime en consola
+ el resultado del ordenamiento. El algoritmo de ordenamiento lo que hace es ordenar
+ una lista de menor a Mayor. y lo hace comparando los primeros 2 valores de la lista,
+ si el primer valor es mayor que el segundo valor entonces se intercambian los valores de la lista.
+ se repite la comparacion hasta llegar al final de la lista.
+
+ '''
+
