7114558313fd1a32614c595dec46745fa6858139
szabo357 <szabo357@gmail.com>
2023-10-21 20:33:46 -0600

    columna excel.py update

--- Ejercicios/11-columna_excel.py
+++ Ejercicios/11-columna_excel.py
@@ -4,17 +4,18 @@
   - Las columnas se designan por letras de la "A" a la "Z" de forma infinita.
   - Ejemplos: A = 1, Z = 26, AA = 27, CA = 79.
 """
+import string as strs
 
 def calculate_excel_column(column_name: str)->int:
      column_number = 0
-     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+     alphabet = strs.ascii_uppercase
      
      for letter in column_name.upper():
         column_number = (column_number * len(alphabet)) + (alphabet.index(letter) + 1)
 
      return column_number  
       
-      
+
 print(calculate_excel_column("AZZ"))
 print(calculate_excel_column("A"))
 print(calculate_excel_column("AA"))
