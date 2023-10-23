0117fb9644f84b8ceef546428d07c0529c154c33
szabo357 <szabo357@gmail.com>
2023-10-22 23:08:39 -0600

    Removiendo elementos de Sets

--- Codigo/sets.py
+++ Codigo/sets.py
@@ -6,18 +6,26 @@ my_other_set = {'items1', 'items2', 'items3', 'items4'}
 print( len(my_set) )
 print( len(my_other_set) )
 
-my_set.add('Libro')
-my_set.add('Lapiz')
-my_set.add('Papel')
-my_set.add('Borrador')
-print(my_set)
 
-my_other_set.add('Libro')
+
+#Agregando elementos a un Set. La funcion Add solo permite agregar un elemento a la vez.
+my_set.add('Libro')
+my_set.add('Libro1')
 print(my_set)
-print(len(my_set))
+my_other_set.add('Libro')
+
+#Update permite agregar varios elementos a la vez. toma una lista de argumentos.
+my_other_set.update(['item5','item6','item7'])
 print(my_other_set)
 
-#verificar si un elemento existe en un set
+
+#Verificando si un elemento existe en un set
 fruits = {'banana', 'orange', 'mango', 'lemon'}
 print('mango' in fruits ) # True
 
+#Remover elementos de un Set
+my_other_set.remove('item2') # remover elemento especifico.
+print(my_other_set)
+
+#Remover un elemento aleatoriamente
+print(my_other_set.pop())
