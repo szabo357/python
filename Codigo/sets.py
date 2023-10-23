975eeae47ab555d35741a1a0b97a958a6ef6827d
szabo357 <szabo357@gmail.com>
2023-10-22 22:36:52 -0600

    sets update

--- Codigo/sets.py
+++ Codigo/sets.py
@@ -5,3 +5,13 @@ my_other_set = {'items1', 'items2', 'items3', 'items4'}
 
 print( len(my_set) )
 print( len(my_other_set) )
+
+my_set.add('Libro')
+my_set.add('Lapiz')
+my_set.add('Papel')
+my_set.add('Borrador')
+print(my_set)
+
+my_set.add('Libro')
+print(my_set)
+print(len(my_set))
