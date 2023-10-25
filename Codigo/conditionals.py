9458c72a049f07e6fc11332c48d0f35863bc0890
szabo357 <szabo357@gmail.com>
2023-10-25 12:16:42 -0600

    Adding else statement example

--- Codigo/conditionals.py
+++ Codigo/conditionals.py
@@ -1,6 +1,16 @@
 # This file is to learn about conditionals
 
+# The if statement is used to test if certain condition is true. if true, then a block of code
+# is executed.
 a = 3
 if a > 0:
     print('A is a positive number')
 # A is a positive number
+
+# The else statement is used to execute another block of code in case the if condition is false.
+a = 2
+if a > 5:
+    print('A is greater than 5')
+else:
+    print('A is less than 5')
+
