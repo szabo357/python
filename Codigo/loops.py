cf788884fede138ad81d2039f2c07adae3fdb2ba
szabo357 <szabo357@gmail.com>
2023-10-29 20:21:41 -0600

    loops update

--- Codigo/loops.py
+++ Codigo/loops.py
@@ -4,8 +4,21 @@
 
 # we use a while loop to execute a block of code
 # repeatedly intil a given condition is satisfied
+
+# Example 1
 contador=0
 while contador < 5:
     print(contador)
     contador+=1
 print("First while execution has finished")
+
+# Example 2
+# break is used to exit the while loop even though
+# the while condition is true.
+contador = 0
+while contador < 10:
+    print(contador)
+    contador+=1
+    if contador == 3:
+        break
+
