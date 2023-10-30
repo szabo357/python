7bdee7743d92f290687069623332977d29f6a444
szabo357 <szabo357@gmail.com>
2023-10-29 19:40:43 -0600

    while loop example

---
+++ Codigo/loops.py
@@ -0,0 +1,11 @@
+# Python has two types of loops
+# while loops
+# for loops
+
+# we use a while loop to execute a block of code
+# repeatedly intil a given condition is satisfied
+contador=0
+while contador < 5:
+    print(contador)
+    contador+=1
+print("First while execution has finished")
