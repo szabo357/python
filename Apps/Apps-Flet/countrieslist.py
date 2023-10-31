874090d1c77d4adb177ea5b76fb1da44e14016a5
szabo357 <szabo357@gmail.com>
2023-10-31 14:30:36 -0600

    image  loading test

--- Apps/Apps-Flet/countrieslist.py
+++ Apps/Apps-Flet/countrieslist.py
@@ -1,24 +1,35 @@
 import flet as ft
-import countriesdata
-w = world
+import countriesdata as cd
+world = cd.World.countries
 
 def main(page: ft.Page):
     page.title = "Flet countries List"
-    page.vertical_alignment=ft.MainAxisAlignment.CENTER
-    page.bgcolor = ft.colors.INDIGO_100
-    txt_number = ft.TextField(value="0",text_align="center",width=100)
-    
+    page.padding = 50
+    page.theme_mode = ft.ThemeMode.DARK
+    page.update()
+   # page.vertical_alignment=ft.MainAxisAlignment.CENTER
     # Agregando elementos a la pagina.
-    page.add(
-        ft.Text(value="Hello Flet",font_family="Roboto",
-            size=20,text_align=ft.MainAxisAlignment.CENTER,
-            weight="Bold",color="blue"
-        ),
-        ft.Divider(height=50,color=ft.colors.BROWN_100),
-        ft.Row(
-          alignment=ft.MainAxisAlignment.CENTER              
-        )            
+    img = ft.Image(
+        src=f"https://restcountries.eu/data/afg.svg",
+        width=100,
+        height=100,
+        fit=ft.ImageFit.CONTAIN,
     )
-    
+    images = ft.Row(expand=1,wrap=False,scroll="always")
+    page.add(img,images)
+
+    for country in world[:30]:
+        images.controls.append(
+            ft.Image(
+                src= country["flag"],
+                width=200,
+                height=200,
+                fit=ft.ImageFit.NONE,
+                repeat=ft.ImageRepeat.NO_REPEAT,
+                border_radius=ft.border_radius.all(10)
+            )    
+        )
+    page.update()
+
 ft.app(target=main)
 #ft.app(target=main,view=ft.AppView.WEB_BROWSER)
