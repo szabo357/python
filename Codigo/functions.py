0138bb9237a0695cfaf226d7e3baea65fea7f319
szabo357 <szabo357@gmail.com>
2023-10-30 22:13:14 -0600

    updated area of a circle

--- Codigo/functions.py
+++ Codigo/functions.py
@@ -8,7 +8,7 @@ def half_triangle(num:int):
         print(my_str)
         my_str+="#"
 
-def area_of_circle (r:int):
+def area_of_circle (r):
     PI = 3.14
     area = PI * r ** 2
     return area
