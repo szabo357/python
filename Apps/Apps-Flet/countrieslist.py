c497b4221734d9c2623cb96782cdf0c52fa74606
szabo357 <szabo357@gmail.com>
2023-10-31 13:58:40 -0600

    countries data has been added

--- Apps/Apps-Flet/countrieslist.py
+++ Apps/Apps-Flet/countrieslist.py
@@ -1,19 +1,11 @@
 import flet as ft
 
 def main(page: ft.Page):
-    page.title = "Flet counter example"
+    page.title = "Flet countries List"
     page.vertical_alignment=ft.MainAxisAlignment.CENTER
     page.bgcolor = ft.colors.INDIGO_100
     txt_number = ft.TextField(value="0",text_align="center",width=100)
     
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value)-1)
-        page.update()
-    
-    def add_click(e):
-        txt_number.value = str(int(txt_number.value)+1)
-        page.update()
-
     # Agregando elementos a la pagina.
     page.add(
         ft.Text(value="Hello Flet",font_family="Roboto",
@@ -22,16 +14,9 @@ def main(page: ft.Page):
         ),
         ft.Divider(height=50,color=ft.colors.BROWN_100),
         ft.Row(
-          [
-            ft.IconButton(icon=ft.icons.REMOVE,on_click=minus_click),
-            txt_number,
-            ft.IconButton(icon=ft.icons.ADD,on_click=add_click)    
-          ],
           alignment=ft.MainAxisAlignment.CENTER              
         )            
     )
     
-
-
 ft.app(target=main)
 #ft.app(target=main,view=ft.AppView.WEB_BROWSER)
