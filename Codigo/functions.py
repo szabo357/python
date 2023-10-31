89396b1985bb5e0d3a76de82b14d84eea70df180
szabo357 <szabo357@gmail.com>
2023-10-30 22:23:37 -0600

    update sum of numbers. bug fix

--- Codigo/functions.py
+++ Codigo/functions.py
@@ -13,7 +13,18 @@ def area_of_circle (r):
     area = PI * r ** 2
     return area
 
-# calling the function
+def sum_of_numbers(n):
+    total = 0
+    for i in range(n+1):
+        total+=i
+    
+    return total
+
+# calling the functions
 half_triangle(10)
 print(area_of_circle(10))
 print(area_of_circle(5))
+print(sum_of_numbers(5))
+#print(sum_of_numbers(10))
+#print(sum_of_numbers(100))
+#print(sum_of_numbers(1000))
