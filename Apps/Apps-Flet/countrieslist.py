2911be182a06ddf1a94ba92a35ade5d1cc6e40fb
szabo357 <szabo357@gmail.com>
2023-10-31 13:30:18 -0600

    countries list

--- Apps/Apps-Flet/countrieslist.py
+++ Apps/Apps-Flet/countrieslist.py
@@ -3,7 +3,7 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Flet counter example"
     page.vertical_alignment=ft.MainAxisAlignment.CENTER
-
+    page.bgcolor =
     txt_number = ft.TextField(value="0",text_align="center",width=100)
     
     def minus_click(e):
@@ -14,7 +14,13 @@ def main(page: ft.Page):
         txt_number.value = str(int(txt_number.value)+1)
         page.update()
 
+    # Agregando elementos a la pagina.
     page.add(
+        ft.Text(value="Hello Flet",font_family="Roboto",
+            size=20,text_align=ft.MainAxisAlignment.CENTER,
+            weight="Bold",color="blue"
+        ),
+        ft.Divider(height=50,color=ft.colors.BROWN_100),
         ft.Row(
           [
             ft.IconButton(icon=ft.icons.REMOVE,on_click=minus_click),
