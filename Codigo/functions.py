9bc66b3aee88b12c691cf662dc0cd541eb25e092
szabo357 <szabo357@gmail.com>
2023-10-30 22:01:02 -0600

    adding custom functions to practice

--- Codigo/functions.py
+++ Codigo/functions.py
@@ -1,2 +1,12 @@
 # created functions file to create
 # custom functions.
+
+# function definition
+def half_triangle(num:int):
+    my_str ="#"
+    for _ in range(num):
+        print(my_str)
+        my_str+="#"
+
+# calling the function
+half_triangle(10)
