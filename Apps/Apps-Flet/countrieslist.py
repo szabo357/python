eef8145c6fe3f56300c9ef978da3a641f27f8baf
szabo357 <szabo357@gmail.com>
2023-10-31 18:07:24 -0600

    update countries list

--- Apps/Apps-Flet/countrieslist.py
+++ Apps/Apps-Flet/countrieslist.py
@@ -8,35 +8,40 @@ def main(page: ft.Page):
     page.padding = 50
     page.theme_mode = ft.ThemeMode.LIGHT
     page.update()
-   # page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+    # definiendo contenedores
     # Agregando elementos a la pagina.
-    img = ft.Image(
-        src=f"https://restcountries.eu/data/afg.svg",
-        width=100,
-        height=100,
-        fit=ft.ImageFit.CONTAIN,
-    )
     images = ft.Row(expand=1,wrap=False,scroll="always")
-    #flags = ft.Row(expand=1,wrap=False,scroll="always")
-    page.add(img,images)
-
-    for country in flg[:30]:
-        images.controls.append([
+    countries = ft.Row(expand=1,wrap=False,scroll="always")
+    page.add(images,countries)
+    honduras ="https://cdn.jsdelivr.net/gh/hampusborgos/country-flags@main/svg/hn.svg"
+    mycontainer = ft.Column(
+        controls=[
             ft.Image(
-                src= country["flag"],
+                src=honduras,
                 width=200,
                 height=200,
                 fit=ft.ImageFit.CONTAIN,
                 repeat=ft.ImageRepeat.NO_REPEAT,
                 border_radius=ft.border_radius.all(10)
             ),
-            ft.Text(
-                value=country["country"],
+            ft.Text(value="Honduras",height=20,weight="Bold")
+        ]
+    )
+
+    for country in flg[:30]:
+        images.controls.append(
+            ft.Image(
+                src=country["flag"],
+                width=200,
                 height=200,
-                bgcolor="blue",
-                selectable=False
-            )    
-        ])
+                fit=ft.ImageFit.CONTAIN,
+                repeat=ft.ImageRepeat.NO_REPEAT,
+                border_radius=ft.border_radius.all(10)
+            )
+        )
+    countries.controls.append(ft.Text(value=country["country"],height=20,weight="Bold"))
+    page.add(mycontainer)
     page.update()
 
 ft.app(target=main)
