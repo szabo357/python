49460d74ee7e67095265d6f6c50cdaf97e306554
szabo357 <szabo357@gmail.com>
2023-11-01 20:59:06 -0600

    bug fix scroll todo app

--- Apps/Apps-Flet/todo_app.py
+++ Apps/Apps-Flet/todo_app.py
@@ -67,13 +67,11 @@ class Task(ft.UserControl):
 class TodoApp(ft.UserControl):
     def build(self):
         self.new_task = ft.TextField(hint_text="¿Qué tareas hay que hacer?",expand=True)
-        self.tasks = ft.Column()
+        self.tasks = ft.Column(scroll=ft.ScrollMode.AUTO,auto_scroll=True)
 
         # application's root control (i.e "view") containing all other controls
         return ft.Column(
             width=600,
-            scroll=ft.ScrollMode.AUTO,
-            auto_scroll=True,
             controls=[
                 ft.Row(
                     controls=[
