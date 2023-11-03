61a4d41753b6054ecb4d62dfa3956dabc61a55ce
szabo357 <szabo357@gmail.com>
2023-11-02 20:15:50 -0600

    Ejercicio ordena la lista Ascendente o descendente

--- Ejercicios/17-ordenala_lista.py
+++ Ejercicios/17-ordenala_lista.py
@@ -1,23 +1,25 @@
+ #* Crea una función que ordene y retorne una matriz de números.
+ #* - La función recibirá un listado (por ejemplo [2, 4, 6, 8, 9]) y un parámetro
+ #*   adicional "Asc" o "Desc" para indicar si debe ordenarse de menor a mayor
+ #*   o de mayor a menor.
+ #* - No se pueden utilizar funciones propias del lenguaje que lo resuelvan
+ #*   automáticamente.
 
 def sortlist(args,order="Asc"):
-    xlist =[]
-    lcopy = args
-    maximum = 0
-
-    if order == "Asc":
-        for arg in args:
-            maximum = 0
-            for litem in lcopy:
-                if litem > maximum:
-                    maximum = litem
-            xlist.append(maximum)
-        return xlist
-    elif order == "Desc":    
-        xlist = [1]
-    
+    n= len(args)        
+    for i in range(n):
+        for j in range(0,n-i-1):
+            if order == "Asc":
+                if args[j] > args[j + 1] :
+                    args[j], args[j + 1] = args[j + 1], args[j]
+            elif order =="Desc":
+                if args[j] < args[j + 1] :
+                    args[j], args[j + 1] = args[j + 1], args[j]
+    return args
+            
 
 
-
+        
 mylist = [20,4,10,5,8,15]
-
 print(sortlist(mylist,"Asc"))
+print(sortlist(mylist,"Desc"))
