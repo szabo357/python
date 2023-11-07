9691007433262e51f385ed40824930fa78e892b5
szabo357 <szabo357@gmail.com>
2023-11-07 11:03:50 -0600

    Update modules testing.py

--- Codigo/moduletesting.py
+++ Codigo/moduletesting.py
@@ -1,8 +1,19 @@
+
 import my_module as md
+
+md.printValue("Test")
+print(md.sumValue(1,2,3))
+
 from my_module import printValue,sumValue
-
-#md.printValue("Test")
-#print(md.sumValue(1,2,3))
-
 printValue("Testing 1")
 sumValue(5,4,1)
+
+import math 
+
+print(math.pi)
+
+from math import pi, tau
+
+print(pi)
+print(tau)
+print(math.pow(2,8))
