64c040e5d7ce979ba94d4808ba7f49ca4c465ee1
szabo357 <szabo357@gmail.com>
2023-11-07 10:21:43 -0600

    Module Example

---
+++ Codigo/moduletesting.py
@@ -0,0 +1,8 @@
+import my_module as md
+from my_module import printValue,sumValue
+
+#md.printValue("Test")
+#print(md.sumValue(1,2,3))
+
+printValue("Testing 1")
+sumValue(5,4,1)
---
+++ Codigo/my_module.py
@@ -0,0 +1,8 @@
+# Example functions of a python module.
+
+def sumValue(numberOne, numberTwo, numberThree):
+    print(numberOne + numberTwo + numberThree)
+
+
+def printValue(value):
+    print(value)
