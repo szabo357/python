1a58dc5befafb208062b0e80774f580d105e47df
szabo357 <szabo357@gmail.com>
2023-11-18 22:06:02 -0600

    multiply iterator value

--- Codigo/Intermedio/01list_comprenhension.py
+++ Codigo/Intermedio/01list_comprenhension.py
@@ -6,3 +6,7 @@ print(my_list)
 # multipliying every value * 2
 my_list = [i * 2 for i in range(8)]
 print(my_list)
+
+# multipliying iterator value
+my_list = [i * i for i in range(8)]
+print(my_list)
