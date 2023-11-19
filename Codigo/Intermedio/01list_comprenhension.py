1ca288addb22e26cd8597bb80226c48172575136
szabo357 <szabo357@gmail.com>
2023-11-18 22:04:11 -0600

    multipliying values

---
+++ Codigo/Intermedio/01list_comprenhension.py
@@ -0,0 +1,8 @@
+# creating lists using list comprenhension
+
+my_list = [i + 1 for i in range(8)]
+print(my_list)
+
+# multipliying every value * 2
+my_list = [i * 2 for i in range(8)]
+print(my_list)
