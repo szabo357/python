b1d981ac5ff16f7fe9c1491dc09f1f1d82857081
szabo357 <szabo357@gmail.com>
2023-11-18 21:52:09 -0600

    adding print_date to dates.py

--- Codigo/Intermedio/00dates.py
+++ Codigo/Intermedio/00dates.py
@@ -1,3 +1,23 @@
 from datetime import datetime
+from datetime import date
+
+now = datetime.now()
+
+def print_date(date):
+    print(date.year)
+    print(date.month)
+    print(date.day)
+    print(date.hour)
+    print(date.minute)
+    print(date.second)
+    print(date.timestamp())
+
+print_date(now)
+
+year_2023 = datetime(2023,1,1)
+
+print_date(year_2023)
 
 print(datetime.now())
+
+# time
