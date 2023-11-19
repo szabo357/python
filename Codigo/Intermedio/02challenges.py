d7196e51a79fa8baa13f924f57e252b9de8d1cb3
szabo357 <szabo357@gmail.com>
2023-11-18 22:24:55 -0600

    fibonacci solved

--- Codigo/Intermedio/02challenges.py
+++ Codigo/Intermedio/02challenges.py
@@ -49,3 +49,15 @@ de Fibonacci empezando en 0.
   la que el siguiente siempre es la suma de los dos anteriores.
   0, 1, 1, 2, 3, 5, 8, 13...
 """
+
+def fibonacci():
+    prev = 0
+    next = 1
+
+    for index in range(0,50):
+        print(prev)
+        fib = prev +  next
+        prev = next
+        next = fib
+
+fibonacci()
