8edc60145a1df4658bf99249d730fd2dfe39b637
szabo357 <szabo357@gmail.com>
2023-11-18 22:21:06 -0600

    fibonacci challenge

--- Codigo/Intermedio/02challenges.py
+++ Codigo/Intermedio/02challenges.py
@@ -39,3 +39,13 @@ def is_anagram(word_1,word_2):
     return sorted(word_1.lower())==sorted(word_2.lower())
 
 print(is_anagram("Amor","Mora"))
+
+
+"""
+LA SUCESIÓN DE FIBONACCI
+Escribe un programa que imprima los 50 primeros números de la sucesión
+de Fibonacci empezando en 0.
+- La serie Fibonacci se compone por una sucesión de números en
+  la que el siguiente siempre es la suma de los dos anteriores.
+  0, 1, 1, 2, 3, 5, 8, 13...
+"""
