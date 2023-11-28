df36d2795525a6dc69305ed6ebf8d1a9823a9d61
szabo357 <szabo357@gmail.com>
2023-11-28 00:07:20 -0600

    Help Topics

--- Codigo/Intermedio/exploring_builtins.py
+++ Codigo/Intermedio/exploring_builtins.py
@@ -387,6 +387,29 @@ break               for                 not
 # **=                 <<                  ^=                  ~
 # *=                  <<=                 _
 
-help('topics')
+#help('topics')
+"""
+Here is a list of available topics.  Enter any topic name to get more help.
 
-#help('modules spam')
+ASSERTION           DELETION            LOOPING             SHIFTING
+ASSIGNMENT          DICTIONARIES        MAPPINGMETHODS      SLICINGS
+ATTRIBUTEMETHODS    DICTIONARYLITERALS  MAPPINGS            SPECIALATTRIBUTES
+ATTRIBUTES          DYNAMICFEATURES     METHODS             SPECIALIDENTIFIERS
+AUGMENTEDASSIGNMENT ELLIPSIS            MODULES             SPECIALMETHODS
+BASICMETHODS        EXCEPTIONS          NAMESPACES          STRINGMETHODS
+BINARY              EXECUTION           NONE                STRINGS
+BITWISE             EXPRESSIONS         NUMBERMETHODS       SUBSCRIPTS
+BOOLEAN             FLOAT               NUMBERS             TRACEBACKS
+CALLABLEMETHODS     FORMATTING          OBJECTS             TRUTHVALUE
+CALLS               FRAMEOBJECTS        OPERATORS           TUPLELITERALS
+CLASSES             FRAMES              PACKAGES            TUPLES
+CODEOBJECTS         FUNCTIONS           POWER               TYPEOBJECTS
+COMPARISON          IDENTIFIERS         PRECEDENCE          TYPES
+COMPLEX             IMPORTING           PRIVATENAMES        UNARY
+CONDITIONAL         INTEGER             RETURNING           UNICODE
+CONTEXTMANAGERS     LISTLITERALS        SCOPING
+CONVERSIONS         LISTS               SEQUENCEMETHODS
+DEBUGGING           LITERALS            SEQUENCES
+"""
+
+help('modules spam')
