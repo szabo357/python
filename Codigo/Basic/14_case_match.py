414b3be5747833cfd0aab0d221496a707207058f
szabo357 <szabo357@gmail.com>
2023-11-27 20:09:45 -0600

    match-case

---
+++ Codigo/Basic/14_case_match.py
@@ -0,0 +1,28 @@
+# match-case Tutorial
+#https://www.geeksforgeeks.org/python-match-case-statement/
+
+user = 'Marco'    
+
+match user:
+    case 'Marvin':
+        print(f"Hello {user} !")
+    case 'Mauricio':
+        print(f"Hello {user} !")
+    case 'Marco':
+        print(f"Hello {user} !")
+    case _ :
+        print('None user')
+
+
+values = 100
+
+match values:
+
+    case values if range(0,101):
+        print(f'Result is { values }') 
+
+    case "100" if 100 in values: 
+        print(f"resultado {values*2}")
+    
+    case values if values > 10 :   
+        print('if comparison')
