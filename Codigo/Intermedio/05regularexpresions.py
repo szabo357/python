e1dbf77b3fdb91d1dfbc67591dd430ceb94eb0ac
szabo357 <szabo357@gmail.com>
2023-11-29 11:40:56 -0600

    update regularexpressions

--- Codigo/Intermedio/05regularexpresions.py
+++ Codigo/Intermedio/05regularexpresions.py
@@ -5,11 +5,12 @@ maillist = []
 
 for linea in man:
     linea = linea.strip()
+#    if re.search("^From:", linea):
     if re.search("From:", linea):
         linea = linea.lstrip("From: ")
         maillist.append(linea)
-        
 
+print("La cantidad de correos es: ", len(maillist))
 maillist = set(maillist)
 maillist = list(maillist)
 
