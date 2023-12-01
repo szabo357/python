41397d42271ef7f574e4a4c662bceedb9dfaaa83
szabo357 <szabo357@gmail.com>
2023-12-01 17:22:41 -0600

    first api wiht fast-api

--- Apps/Fast-Api-Tutorial/first-api.py
+++ Apps/Fast-Api-Tutorial/first-api.py
@@ -1,8 +1,39 @@
-import fastapi as fa
+from typing import Union
 
-print('hello world')
+from fastapi import FastAPI
 
-print('hello world')
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
 
-print('hello world')
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+"""
+To run the server
+
+enter the following command in the terminal:
+uvicorn first-api --reloaad
+
+Open your browser at http://127.0.0.1:8000/items/5?q=somequery.
+
+You will see the JSON response as:
+
+You already created an API that:
+
+Receives HTTP requests in the paths / and /items/{item_id}.
+Both paths take GET operations (also known as HTTP methods).
+The path /items/{item_id} has a path parameter item_id that should be an int.
+The path /items/{item_id} has an optional str query parameter q.
+
+Interactive API docs
+Now go to http://127.0.0.1:8000/docs.
+
+You will see the automatic interactive API documentation (provided by Swagger UI)
+
+"""
