07559d9fd648de759de7aaf9da1ce98f33444c99
szabo357 <szabo357@gmail.com>
2023-12-02 22:56:41 -0600

    order of declaration matters

--- Apps/Fast-Api-Tutorial/main.py
+++ Apps/Fast-Api-Tutorial/main.py
@@ -21,3 +21,20 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+# order matters 
+"""When creating path operations, you can find situations where you have a fixed path.
+Like /users/me, let's say that it's to get data about the current user.
+And then you can also have a path /users/{user_id} to get data about a specific user by some user ID.
+Because path operations are evaluated in order, you need to make sure that the path 
+for /users/me is declared before the one for /users/{user_id}:"""
+
+@app.get("users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
