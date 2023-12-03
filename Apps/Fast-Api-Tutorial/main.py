47ebe3dfd2de3b8b15729007ec3cc67cff7045b8
szabo357 <szabo357@gmail.com>
2023-12-02 22:27:11 -0600

    Path with parameters or variables

--- Apps/Fast-Api-Tutorial/main.py
+++ Apps/Fast-Api-Tutorial/main.py
@@ -9,3 +9,9 @@ async def root():
     """Root endpoint of out API
     it returns "Hello World!" """
     return {"message": "Hello World!"}
+
+# Declaring Path with parameters or variables
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
