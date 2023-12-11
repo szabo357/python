9c62f1f2b70d409916cb13e1b77f6b922717bf1e
szabo357 <szabo357@gmail.com>
2023-12-11 13:37:50 -0600

    sqlite tutorial connection close

--- Codigo/LearningSqlite/sqlitetutorial.py
+++ Codigo/LearningSqlite/sqlitetutorial.py
@@ -36,6 +36,14 @@ query = "SELECT year, title FROM movie ORDER BY year"
 for row in cur.execute(query):
     print(row)
 
+# close connection to make sure db has been written to disk.
+con.close()
+
+new_con = sqlite3.connect("tutorial.db")
+new_cur = new_con.cursor()
+res = new_cur.execute("SELECT title, year FROM movie ORDER BY score DESC")
+title, year = res.fetchone()
+print(f'The highest scoring Monty Python movie is {title!r}, released in {year}')
 
 
 chdir(savedpath)
